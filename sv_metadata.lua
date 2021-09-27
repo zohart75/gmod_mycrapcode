28f7f34e0d074c27a0e6cbc9af64706c84d784ec
Deviatt <70473864+Deviatt@users.noreply.github.com>
2021-09-27 21:55:50 +0300

    Add no meta functions

--- sv_metadata.lua
+++ sv_metadata.lua
@@ -6,6 +6,16 @@ hook.Add("Initialize", "Metadata.Create", function()
 	query("CREATE TABLE IF NOT EXISTS metadata(info, key, type, value)")
 end)
 
+hook.Add("PlayerInitialSpawn", "Metadata.Load", function(ply)
+	local info = ply:SteamID().."["..ply:Name().."]"
+
+	local data = query("SELECT * FROM metadata WHERE info = "..str(info))
+
+	for _, v in pairs(data) do
+		ply:SetNWString("metadata_"..v.key, v.value)
+	end
+end)
+
 function PLAYER:SetMetadata(key, value)
     if (!key or !value) then return end
 
@@ -40,6 +50,50 @@ end
 function PLAYER:GetMetadata(key)
 	if (!key) then return end
 	local info = self:SteamID().."["..self:Name().."]"
+
+	local data = query("SELECT * FROM metadata WHERE info = "..str(info).." AND key = "..str(key))
+	if (data) then
+		local val = sql.QueryValue("SELECT value FROM metadata WHERE info = "..str(info).." AND key = "..str(key))
+		return (data[1].type == "table" and unjson(val) or val)
+	end
+end
+
+--[[ No Meta ]]
+
+function SetMetadata(ply, key, value)
+    if (!ply or !key or !value) then return end
+
+    local info = ply:SteamID().."["..ply:Name().."]"
+    local type = type(value)
+
+    value = istable(value) and json(value) or tostring(value)
+
+    ply:SetNWString("metadata_"..key, value)
+
+    local data = query("SELECT * FROM metadata WHERE info = "..str(info).." AND key = "..str(key))
+    if (data) then
+        query("UPDATE metadata SET type = "..str(type)..", value = "..str(value).." WHERE info = "..str(info))
+    else
+        query("INSERT INTO metadata(info, key, type, value) VALUES("..str(info)..", "..str(key)..", "..str(type)..", "..str(value)..")")
+    end
+end
+
+function DeleteMetadata(ply, key)
+	if (!ply or !key) then return end
+
+	local info = ply:SteamID().."["..ply:Name().."]"
+
+	ply:SetNWString("metadata_"..key, nil)
+
+	local data = query("SELECT * FROM metadata WHERE info = "..str(info).." AND key = "..str(key))
+	if (data) then
+		query("DELETE FROM metadata WHERE info = "..str(info).." AND key = "..str(key))
+	end
+end
+
+function GetMetadata(ply, key)
+	if (!ply or !key) then return end
+	local info = ply:SteamID().."["..ply:Name().."]"
 
 	local data = query("SELECT * FROM metadata WHERE info = "..str(info).." AND key = "..str(key))
 	if (data) then
